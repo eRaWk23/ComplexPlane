abc42e6fe44af98c464974ce71781d1b7e8b33a9
Your Name <you@example.com>
2024-04-20 07:10:31 -0700

    Completed .h file? Started .cpp

--- complexPlane.cpp
+++ complexPlane.cpp
@@ -1,1 +1,21 @@
 #include"complexPlane.h"
+
+ComplexPlane::ComplexPlane(int pixelSize, int pixelHeight)
+{
+    m_pixel_size = {pixelSize, pixelHeight};
+    m_plane_center = {0,0};
+    m_plane_size = {BASE_WIDTH,BASE_HEIGHT * m_aspectRatio};
+    m_zoomCount = 0;
+    m_state = State::CALCULATING;
+    // Init VertexArray
+}
+        
+void updateRender();
+void zoomIn();
+void zoomOut();
+void setCenter(Vector2i mousePixel);
+void setMouseLocation(Vector2i mousePixel);
+void loadText(Text& Text);
+size_t countIterations(Vector2f coord);
+Vector2f mapPixelToCoords(Vector2i mousePixel);
+void iterationToRGB(size_t count, Uint8& g, Uint8& b);
--- complexPlane.h
+++ complexPlane.h
@@ -1,19 +1,28 @@
 #ifndef COMPLEXPLANE_H
 #define COMPLEXPLANE_H
 
-#include<iostream>
-#include<vector>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace sf;
+using namespace std;
 
 const unsigned int MAX_ITER = 64;
 const float BASE_WIDTH = 4.0;
 const float BASE_HEIGHT = 4.0;
 const float BASE_ZOOM = 0.5;
 
+enum class State 
+{
+    CALCULATING,
+    DISPLAYING
+};
+
 class ComplexPlane : public sf::Drawable
 {
     public:
         ComplexPlane(int pixelSize, int pixelHeight);
-        void draw(RenderTarger& target, RenderStates states) const
+        void draw(RenderTarget& target, RenderStates states) const
         {
             target.draw(m_vArray);
         }
@@ -27,7 +36,14 @@ class ComplexPlane : public sf::Drawable
         size_t countIterations(Vector2f coord);
         Vector2f mapPixelToCoords(Vector2i mousePixel);
         void iterationToRGB(size_t count, Uint8& g, Uint8& b);
-
+        VertexArray m_vArray;
+        State m_state;
+        Vector2i m_mouseLocation;
+        Vector2i m_pixel_size;
+        Vector2f m_plane_center;
+        Vector2f m_plane_size;
+        int m_zoomCount;
+        float m_aspectRatio;
 };     
 
 #endif 
