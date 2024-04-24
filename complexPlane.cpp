6f92a8b57c78fed069afe9be8a617b1bcc704c07
Your Name <you@example.com>
2024-04-23 19:06:48 -0700

    working on Project

--- complexPlane.cpp
+++ complexPlane.cpp
@@ -3,14 +3,33 @@
 ComplexPlane::ComplexPlane(int pixelSize, int pixelHeight)
 {
     m_pixel_size = {pixelSize, pixelHeight};
+    m_aspectRatio = static_cast<float>(pixelHeight)/pixelSize;
     m_plane_center = {0,0};
     m_plane_size = {BASE_WIDTH,BASE_HEIGHT * m_aspectRatio};
     m_zoomCount = 0;
     m_state = State::CALCULATING;
     // TODO - Init VertexArray
+    m_vArray.setPrimitiveType(sf::Points);
+    m_vArray.resize(pixelSize * pixelHeight);
 }
         
-void updateRender();
+void ComplexPlane::updateRender()
+{
+    if(m_state == State::CALCULATING)
+    {
+        for (int i = 0; i < m_pixel_size.y; i++)
+        {
+            for (int j = 0; j < m_pixel_size.x; j++)
+            {
+                m_vArray[j + i *m_pixel_size.x].position = sf::Vector2f(j, i);
+                sf::Vector2f coord = mapPixelToCoords(sf::Vector2i(j,i));
+                size_t iterations = countIterations(coord);
+                Uint8 r,g,b;
+                iterationToRGB(iterations, r, g, b);
+            }
+        }
+    }
+}
 void zoomIn();
 void zoomOut();
 void setCenter(Vector2i mousePixel);
